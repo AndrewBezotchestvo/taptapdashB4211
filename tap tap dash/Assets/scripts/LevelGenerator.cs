f375bb1d20a9d3350e08a385e232e7fd093f8300
Andrew Bezotchestvo <03051820dr@gmail.com>
2025-04-02 00:33:35 +0800

    fixed

--- tap tap dash/Assets/scripts/LevelGenerator.cs
+++ tap tap dash/Assets/scripts/LevelGenerator.cs
@@ -8,35 +8,54 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField] private Transform _player;
     [SerializeField] private Transform _startPoint;
 
-    [SerializeField] private List<GameObject> _chunks;
+    [SerializeField] private List<GameObject> _verticalChunks;
+    [SerializeField] private List<GameObject> _horizontalChunks;
+
+    [SerializeField] private float _triggerDistance;
+
     private List<GameObject> _spawnedchunks;
 
     private GameObject startChunkPoint;
     private GameObject endChunkPoint;
 
     private Vector3 _startNextPoint;
+
+    private bool _isVertical = false;
+
     void Start()
     {
+        _isVertical = false;
         _startNextPoint = SpawnNewChunk(_startPoint.position);
     }
 
     void Update()
     {
-        
+        if (Vector3.Distance(_startNextPoint, _player.position) < _triggerDistance)
+        {
+            _startNextPoint = SpawnNewChunk(_startNextPoint);
+            _isVertical = !_isVertical;
+        }
     }
 
     private Vector3 SpawnNewChunk(Vector3 startPoint)
     {
-        GameObject newChunk = Instantiate(_chunks[Random.Range(0, _chunks.Count)]); //создать рандомный элемент из списка наших чанков
+        GameObject newChunk;
+
+        if (!_isVertical)
+        {   //создать рандомный элемент из списка наших горизонтальных чанков
+            newChunk = Instantiate(_horizontalChunks[Random.Range(0, _horizontalChunks.Count)]); 
+        }
+        else
+        {   //создать рандомный элемент из списка наших вертикальных чанков
+            newChunk = Instantiate(_verticalChunks[Random.Range(0, _verticalChunks.Count)]); 
+        }
 
         startChunkPoint = newChunk.transform.GetChild(0).gameObject;
         endChunkPoint = newChunk.transform.GetChild(1).gameObject;
 
-        newChunk.transform.position = startPoint + (transform.position + startChunkPoint.transform.localPosition);
+        newChunk.transform.position = startPoint - startChunkPoint.transform.localPosition;
 
         startPoint = endChunkPoint.transform.position;
-
-        _spawnedchunks.Add(newChunk);
         return startPoint;
     }
 }
